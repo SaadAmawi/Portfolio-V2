45b136ba2fd4736258e32a130c653e01f36f873c
Saad Amawi <saadamawi@yahoo.com>
2025-10-11 00:44:32 +0400

    Done with Hero Page

--- portfolio-v2/src/Components/Header/index.jsx
+++ portfolio-v2/src/Components/Header/index.jsx
@@ -1,6 +1,7 @@
 import HeaderButton from "../HeaderButton"
 import './index.css'
 import image from '../Assets/Images/logo2.png'
+import WhiteButton from "../WhiteButton";
 function Header(){
 
     return(
@@ -11,11 +12,12 @@ function Header(){
         <h1 className="big"> A</h1> */}
         <img src={image} alt="logo" className="logo-image"/>
         </div>
-        <div className="right-btns">
+        <div className="middle-btns">
         <HeaderButton toLink="/about" buttonText="About Me"/>
         <HeaderButton toLink="/projects" buttonText="Projects"/>
         <HeaderButton toLink="/mygpt" buttonText="MyGPT"/>
         </div>
+        <WhiteButton toLink="/mygpt" buttonText="Contact" cName="filled-header"/>
     </nav>
     </div>)
 }
