dc4a3b14dce3b3381d06926032b655cbfe8e0d94
Saad Amawi <saadamawi@U-1F-0111.local>
2025-11-06 14:55:55 +0400

    Mobile Responsive Design

--- portfolio-v2/src/Components/Experience/index.js
+++ portfolio-v2/src/Components/Experience/index.js
@@ -2,19 +2,17 @@ import React, { useRef } from 'react'
 import './index.css'
 import Item from './Item'
 import { useToggle } from '../../toggleContext'
-import { useTheme } from '../../themeContext'
 
 
 function Experience() {
-  const {theme} = useTheme()
   const bodyRef = useRef(null)
   const {toggledSection,setToggledSection} = useToggle();
 
 
 
   return (
-    <div className='experience-block' id="experience" >
-    <div className={`experience ${theme}`} ref={bodyRef}>
+    // <div className='experience-block' id="experience" >
+    <div className={`experience`} ref={bodyRef}>
         <div className='toggle'>
             <button className={toggledSection==="Work"?"toggleButtonOn work":"toggleButtonOff" } onClick={()=>setToggledSection("Work")}>Work</button>
             <button className={toggledSection==="Experience"?"toggleButtonOn education":"toggleButtonOff"} onClick={()=>setToggledSection("Experience")}>Education</button>
@@ -23,7 +21,7 @@ function Experience() {
             <Item active={toggledSection}/>
             </div>
     </div>
-  </div>
+  // </div>
   )
 }
 
